5d5a8a3bf48c272a2cddca7cc0f11fecf095afb7
thumberts <tony.humberts@gmail.com>
2019-03-09 11:28:38 -0500

    minor

--- cong-viewer/lib/all_nodes_app/all_nodes_app.dart
+++ cong-viewer/lib/all_nodes_app/all_nodes_app.dart
@@ -5,6 +5,7 @@ import 'package:intl/intl.dart';
 import 'package:date/date.dart';
 import 'package:date/src/term_parse.dart';
 import 'package:http/http.dart';
+import 'package:timeseries/timeseries.dart';
 import 'package:plotly/plotly.dart';
 import 'package:timezone/browser.dart';
 import 'package:elec/src/iso/iso.dart';
@@ -16,12 +17,12 @@ import 'package:elec_server/src/ui/term_input.dart';
 
 class AllNodesApp {
 
-  var cache = <int,List<Map<String,dynamic>>>{};
+  var cache = <int,TimeSeries<num>>{};
 
   html.Element wrapper;
 
   TermInput _termInput;
-  TermParser _parser;
+
 
   /// Visualize the congestion for all the nodes in the pool at once
   AllNodesApp(this.wrapper,
@@ -41,13 +42,19 @@ class AllNodesApp {
   void _addHtmlControls() {
     wrapper.setAttribute('style', 'margin-left: 15px');
 
-    _termInput = TermInput(wrapper);
+    _termInput = TermInput(wrapper, defaultValue: '-40d');
+
 
   }
 
 
   Controller _makeController() {
 
+    var filters = <String,dynamic>{
+      'term': _termInput.value,
+    };
+
+    return Controller(filters: filters);
   }
 
 
--- cong-viewer/lib/all_nodes_app/all_nodes_lib.dart
+++ cong-viewer/lib/all_nodes_app/all_nodes_lib.dart
@@ -3,6 +3,9 @@ library all_nodes_app.lib;
 import 'package:date/date.dart';
 import 'package:fixnum/fixnum.dart';
 import 'package:elec_server/src/generated/timeseries.pbgrpc.dart';
+import 'package:elec_server/client/isoexpress/dalmp.dart';
+import 'package:elec/src/common_enums.dart' as ce;
+
 
 Future<NumericTimeSeries> getSeries(
     int ptid, Date start, Date end, LmpClient client) async {
@@ -16,6 +19,15 @@ Future<NumericTimeSeries> getSeries(
     ..component = congestion;
 
   var response = await client.getLmp(request);
+
+  return response;
+}
+
+Future<NumericTimeSeries> getDailySeries(Date start, Date end,
+    DaLmp client) async {
+
+  var response = await client.getDailyPricesAllNodes(ce.LmpComponent.congestion,
+    start, end);
 
   return response;
 }
