39f064cdc16ab9f8aeed0ea3d97ceb76476cce2c
thumberts <tony.humberts@gmail.com>
2019-05-19 17:00:57 -0400

    fix issues timezone|standalone

--- load-viewer/web/main.dart
+++ load-viewer/web/main.dart
@@ -2,19 +2,18 @@
 
 import 'dart:html';
 import 'package:timezone/browser.dart';
-//import 'package:load_viewer/shape_viewer/shape_viewer.dart';
+import 'package:load_viewer/shape_viewer/shape_viewer.dart';
 import 'package:elec_server/src/utils/custom_client.dart';
 
 
 main() async {
   await initializeTimeZone();
 
-//  var client = CustomClient();
-//  var rootUrl = "http://localhost:8080/";
-//  var wrapper = querySelector('#wrapper-hourly-shape');
-//
-//  var shapeViewer = ShapeViewer(wrapper, client, rootUrl: rootUrl);
-//  await shapeViewer.show();
+  var client = CustomClient();
+  var rootUrl = "http://localhost:8080/";
+  var wrapper = querySelector('#wrapper-hourly-shape');
+  var shapeViewer = ShapeViewer(wrapper, client, rootUrl: rootUrl);
+  await shapeViewer.show();
 
   querySelector('#output').text = 'Your Dart app is running.';
 
